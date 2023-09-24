03298d51fcc8de1ec90988fc5b3b892243ac8a8e
Michael Joy <kodiakchaos@gmail.com>
2023-09-24 13:25:24 -0400

    initial commit

---
+++ src/constants/global.ts
@@ -0,0 +1,18 @@
+export const AppName = 'Web App Template'
+
+export const AppDescription = `
+${AppName} is a Vue 3 Typescript project created by michael-255 on GitHub.
+This project was built to be the foundation for many web applications.
+It is publicly available using the MIT License.
+`
+
+/**
+ * @see https://quasar.dev/style/color-palette
+ */
+export const AppHeaderColor = 'primary'
+
+/**
+ * Used as part of the name of the database in the browser.
+ * - Change after deployment if you want to force database to be recreated
+ */
+export const AppDatabaseVersion = 1
